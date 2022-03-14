aac03399c6b494eb26108394afef5d720a59cc2e
nofind <2868509713@qq.com>
2022-03-14 17:59:01 +0800

    A native version.

--- src/main/java/com/example/UserSystem/Servlets/LoginServlet.java
+++ src/main/java/com/example/UserSystem/Servlets/LoginServlet.java
@@ -3,10 +3,8 @@ package com.example.UserSystem.Servlets;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.sql.*;
 
 /**
  * @author dongyudeng
@@ -15,18 +13,34 @@ import java.util.*;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Map<String,String> map=new HashMap<>();
-        BufferedReader reader=new BufferedReader(new FileReader("user.txt"));
-        String user=null;
-        while((user=reader.readLine())!=null){
-            String regex=",";
-            String[] users=user.split(regex);
-            map.put(users[0],users[1]);
+        String jdbcUrl = "jdbc:mysql://localhost:3306/UserSystem",jdbcUser="user",jdbcPassWord="userpassword";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-        String userName=request.getParameter("userName"),passWord=request.getParameter("passWord");
-        if(map.containsKey(userName)&&map.get(userName).equals(passWord)){
-            request.getSession().setAttribute("userName",userName);
-            response.sendRedirect("/UserSystem/hello.jsp");
+        try(Connection connection=DriverManager.getConnection(jdbcUrl,jdbcUser,jdbcPassWord)){
+            String userName=request.getParameter("userName"),passWord=request.getParameter("passWord");
+            try(Statement statement=connection.createStatement()){
+                try(ResultSet resultSet=statement.executeQuery("SELECT passWord FROM users WHERE name='"+userName+"'")){
+                    String expectedPassWord = null;
+                    if(resultSet.next()){
+                        expectedPassWord=resultSet.getString(1);
+                    }
+                    if(expectedPassWord!=null&&passWord.equals(expectedPassWord)){
+                        request.getSession().setAttribute("userName",userName);
+                        response.sendRedirect("/UserSystem/hello.jsp");
+                    }
+                    else {
+                        request.setAttribute("flag",true);
+                        response.sendRedirect("/UserSystem/login.jsp");
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            request.setAttribute("flag",true);
+            response.sendRedirect("/UserSystem/login.jsp");
         }
     }
 }
--- src/main/java/com/example/UserSystem/Servlets/RegisterServlet.java
+++ src/main/java/com/example/UserSystem/Servlets/RegisterServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.*;
+import java.sql.*;
 
 /**
  * @author dongyudeng
@@ -12,9 +13,23 @@ import java.io.*;
 public class RegisterServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter writer=new PrintWriter("user.txt");
-        String userName=request.getParameter("userName"),passWord=request.getParameter("passWord"),gender=request.getParameter("gender");
-        writer.println(userName + "," + passWord + "," + gender);
+        String jdbcUrl = "jdbc:mysql://localhost:3306/UserSystem",jdbcUser="user",jdbcPassWord="userpassword";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try(Connection connection= DriverManager.getConnection(jdbcUrl,jdbcUser,jdbcPassWord)){
+            String userName=request.getParameter("userName"),passWord=request.getParameter("passWord"),gender=request.getParameter("gender");
+            try(PreparedStatement preparedStatement=connection.prepareStatement("INSERT INTO users (name,passWord,gender) VALUES (?,?,?)")){
+                preparedStatement.setObject(1,userName);
+                preparedStatement.setObject(2,passWord);
+                preparedStatement.setObject(3,gender);
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         response.sendRedirect("/UserSystem/login.jsp");
     }
 }
